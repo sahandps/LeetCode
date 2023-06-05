5a188c50b49658d1fbe026795ee16ddaf9eb62db
Sahand <sahand.prs@gmail.com>
2023-06-05 12:37:55 +0200

    working on it

--- Arrays 101/src/MaxConsecutiveOnes.java
+++ Arrays 101/src/MaxConsecutiveOnes.java
@@ -1,23 +1,19 @@
 public class MaxConsecutiveOnes {
     public static void main(String[] args) {
-        int[] nums = {1,1,0,1,1,1};
+        int[] nums = {1, 1, 0, 1, 1, 1};
         findMaxConsecutiveOnes(nums);
     }
 
 
     public static int findMaxConsecutiveOnes(int[] nums) {
         int counter = 0;
-        for (int i = 0; i < nums.length; i++) {
-
-            if(nums[i] == 0 && nums[i + 1] + nums[i + 2] == 2 || nums[i] == 1 && nums[i +1] == 1){
-                counter = counter + 1;
-            }
-            if(i == nums.length-2){
-                break;
-            }
-
+        for (int i = 1; i < nums.length; i++) {
+            if(num[i - 1] == 1 && nums[i] == 1){
+                counter++;
         }
-        System.out.println(counter);
-        return counter - 1;
+
     }
+        System.out.println(counter);
+        return counter;
+}
 }
