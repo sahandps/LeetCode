adf9a3a1c0315b9ec4c04524ff789527409d1f0b
Sahand <sahand.prs@gmail.com>
2023-06-07 09:52:25 +0200

    Solution Accepted

--- Arrays 101/src/FindNumberswithEvenNumberofDigits.java
+++ Arrays 101/src/FindNumberswithEvenNumberofDigits.java
@@ -1,16 +1,16 @@
+import java.lang.Math;
 public class FindNumberswithEvenNumberofDigits {
     public static void main(String[] args) {
-        
+        int [] nums = {12,345,2,6,7896};
+        findNumbers(nums);
     }
 
-
-    public int findNumbers(int[] nums) {
+    public static int findNumbers(int[] nums) {
         int counter = 0;
+        int number = 0;
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i] / 10 < 100){
-                counter++;
-            }
-            
+            number = (int) (Math.log10(nums[i]) + 1);
+            System.out.println(number);
         }
         return counter;
     }
