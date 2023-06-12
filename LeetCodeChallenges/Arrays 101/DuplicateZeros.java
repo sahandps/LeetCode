6892cc5c8d750fe0fb23cf577f912afc7344eee0
Sahand <sahand.prs@gmail.com>
2023-06-12 12:33:54 +0200

    Fixing some stuff

--- LeetCodeChallenges/Arrays 101/DuplicateZeros.java
+++ LeetCodeChallenges/Arrays 101/DuplicateZeros.java
@@ -6,13 +6,19 @@ public class DuplicateZeros {
 
     public static void duplicateZeros(int[] arr) {
         int temp = 0;
+        int temp2 = 0;
+        int index = 0;
+        int[] tempArr = new int[arr.length];
 
-        for (int i = 1; i < arr.length; i++) {
-            if(arr[i - 1] == 0){
-                arr[i] = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i < arr.length - 1) {
+                if (arr[i] == 0) {
+                    temp = arr[i + 1];
+                    tempArr[i + 1] = 0;
+                } 
             }
 
-            System.out.println(arr[i]);
+            System.out.println(tempArr[i]);
         }
 
     }
