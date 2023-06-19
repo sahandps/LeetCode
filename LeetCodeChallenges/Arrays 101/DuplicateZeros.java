7915e363849317f7286677357d54383f29aa0a67
Sahand <sahand.prs@gmail.com>
2023-06-19 10:40:33 +0200

    Solved The challenge

--- LeetCodeChallenges/Arrays 101/DuplicateZeros.java
+++ LeetCodeChallenges/Arrays 101/DuplicateZeros.java
@@ -6,21 +6,20 @@ public class DuplicateZeros {
 
     public static void duplicateZeros(int[] arr) {
         int temp = 0;
-        int temp2 = 0;
-        int index = 0;
-        int[] tempArr = new int[arr.length];
-
-        for (int i = 0; i < arr.length; i++) {
-            if (i < arr.length - 1) {
-                if (arr[i] == 0) {
-                    temp = arr[i + 1];
-                    tempArr[i + 1] = 0;
-                } 
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if(arr[i] == 0 && i < arr.length - 1){
+                temp = i;
+                rotateToTheRigh(arr, temp);
+                arr[i] = 0;
             }
 
-            System.out.println(tempArr[i]);
         }
 
     }
 
+    public static void rotateToTheRigh(int[] arr, int n) {
+        for (int j = arr.length - 1; j > n; j--) {
+            arr[j] = arr[j - 1];
+        }
+    }
 }
