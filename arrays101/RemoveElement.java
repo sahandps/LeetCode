d6e34431c25f4efc119d89efe195a08fd1afe406
Sahand <sahand.prs@gmail.com>
2023-07-09 22:52:21 +0200

    Remove element Done

--- arrays101/RemoveElement.java
+++ arrays101/RemoveElement.java
@@ -2,6 +2,46 @@ package arrays101;
 
 public class RemoveElement {
     public static void main(String[] args) {
-        System.out.println("sahand");
+        int[] nums = { 3, 2, 2, 3 };
+
+        removeElement(nums, 3);
     }
+
+    public static int removeElement(int[] nums, int val) {
+        int counter = 0;
+        int newArraySize = 0;
+        int numbers = 0;
+        boolean isOn = true;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                counter++;
+            }
+        }
+
+        newArraySize = nums.length - counter;
+        counter = 0;
+        int[] temp = new int[newArraySize];
+
+        while (isOn) {
+
+            if (nums[counter] != val) {
+                temp[numbers] = nums[counter];
+                counter++;
+                numbers++;
+            } else {
+                counter++;
+            }
+
+            if (counter == nums.length - 1) {
+                isOn = false;
+            }
+
+        }
+        for (int i = 0; i < temp.length; i++) {
+            System.out.println(temp[i]);
+        }
+        return temp.length;
+    }
+
 }
