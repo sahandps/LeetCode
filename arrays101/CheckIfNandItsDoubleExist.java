419829bc932e4b925137731087e9a33ffbb6d064
Sahand Poursadeghi <sahand.prs@gmail.com>
2023-07-11 10:57:00 +0200

    started with the challenge

---
+++ arrays101/CheckIfNandItsDoubleExist.java
@@ -0,0 +1,18 @@
+package arrays101;
+
+public class CheckIfNandItsDoubleExist {
+    public static void main(String[] args) {
+        int [] arr = {10,2,5,3};
+    }
+
+
+
+    public static boolean checkIfExist(int[] arr) {
+
+        
+
+        return false;
+        
+    }
+    
+}
