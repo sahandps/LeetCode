2c707c40ac8da34c4bc81bfd46503bb6729f3f5b
Sahand Poursadeghi <sahand.prs@gmail.com>
2023-07-11 12:22:38 +0200

    workin on it

--- arrays101/CheckIfNandItsDoubleExist.java
+++ arrays101/CheckIfNandItsDoubleExist.java
@@ -2,17 +2,31 @@ package arrays101;
 
 public class CheckIfNandItsDoubleExist {
     public static void main(String[] args) {
-        int [] arr = {10,2,5,3};
+        int[] arr = { -2,0,10,-19,4,6,-8};
+        checkIfExist(arr);
     }
-
-
 
     public static boolean checkIfExist(int[] arr) {
 
-        
+        boolean status = false;
 
-        return false;
-        
+        for (int i = 0; i < arr.length; i++) {
+            if(status == true){
+                break;
+            }
+            for (int j = 1; j < arr.length; j++) {
+                if (arr[i] * 2 == arr[j] || arr[j] * 2 == arr[i]) {
+                    status = true;
+                    break;
+                } else {
+                    status = false;
+                }
+            }
+
+        }
+        System.out.println(status);
+        return status;
+
     }
-    
+
 }
