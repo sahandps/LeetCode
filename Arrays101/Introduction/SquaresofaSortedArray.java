325ac772191ab9fdd6df9e1c94798b5edc4baa47
Sahand <sahand.prs@gmail.com>
2024-03-27 08:44:39 +0100

    Done

---
+++ Arrays101/Introduction/SquaresofaSortedArray.java
@@ -0,0 +1,19 @@
+package Arrays101.Introduction;
+
+import java.util.Arrays;
+
+public class SquaresofaSortedArray {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+
+    }
+}
