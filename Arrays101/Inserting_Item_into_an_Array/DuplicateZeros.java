0bbac52521f0b7d551eff94eeed13b5452ab2e55
Sahand <sahand.prs@gmail.com>
2024-04-29 10:09:40 +0200

    Done

--- Arrays101/Inserting_Item_into_an_Array/DuplicateZeros.java
+++ Arrays101/Inserting_Item_into_an_Array/DuplicateZeros.java
@@ -1,7 +1,33 @@
 package Inserting_Item_into_an_Array;
 
 public class DuplicateZeros {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int[] arr = { 1, 0, 2, 3, 0, 4, 5, 0 };
+        duplicateZeros(arr);
+    }
+
+
+    public static void duplicateZeros(int[] arr) {
+        int i = 0;
+        int j = 1;
+        int tempHolder = 0;
+
+
+        while(i < arr.length){
+
+
+            if(arr[i] == 0){
+                for (int k = arr.length - 1; k > i; k--) {
+
+                    arr[k] = arr[k - 1];
+
+                }
+                i++;
+                arr[i] = 0;
+
+            }
+            i++;
+        }
     }
 }
