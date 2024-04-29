0181db3766b55c72ef8769fa72c1ceb54fb6124f
Sahand <sahand.prs@gmail.com>
2024-04-29 10:12:11 +0200

    Still trying to fix

--- Arrays101/Inserting_Item_into_an_Array/DuplicateZeros.java
+++ Arrays101/Inserting_Item_into_an_Array/DuplicateZeros.java
@@ -3,52 +3,24 @@ package Inserting_Item_into_an_Array;
 public class DuplicateZeros {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         int[] arr = { 1, 0, 2, 3, 0, 4, 5, 0 };
         duplicateZeros(arr);
-    }
-
-
-    public static void duplicateZeros(int[] arr) {
-        int i = 0;
-        int j = 1;
-        int tempHolder = 0;
-
-
-        while(i < arr.length){
-
-
-            if(arr[i] == 0){
-                for (int k = arr.length - 1; k > i; k--) {
-
-                    arr[k] = arr[k - 1];
-
-                }
-                i++;
-                arr[i] = 0;
-
-            }
-            i++;
-        }
-=======
-        int [] arr = {1,0,2,3,0,4,5,0};
-        duplicateZeros(arr);
->>>>>>> fa5415ad4a807e33c9eab48f78a80497812bbed9
+        System.out.println("Hiiii");
     }
 
     public static void duplicateZeros(int[] arr) {
-        int i = 0;
-
-        while (i < arr.length) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0) {
-                for (int j = arr.length-1; i < j; j--) {
-                    arr[j + 1] = arr[j];
+                // Shift elements to the right
+                for (int j = arr.length - 1; j > i; j--) {
+                    arr[j] = arr[j - 1];
                 }
-                i++;
-                arr[i] = 0;
+                // Place another zero after the current one
+                if (i < arr.length - 1) {
+                    arr[++i] = 0;
+                }
             }
-            i++;
         }
     }
-
+    
 }
