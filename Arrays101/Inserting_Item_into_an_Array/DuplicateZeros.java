c9f78dc7e0937efea88d2d3a683fc3c327d3e70f
Sahand <sahand.prs@gmail.com>
2024-04-30 08:41:08 +0200

    This one is done too

--- Arrays101/Inserting_Item_into_an_Array/DuplicateZeros.java
+++ Arrays101/Inserting_Item_into_an_Array/DuplicateZeros.java
@@ -5,7 +5,6 @@ public class DuplicateZeros {
     public static void main(String[] args) {
         int[] arr = { 1, 0, 2, 3, 0, 4, 5, 0 };
         duplicateZeros(arr);
-        System.out.println("Hiiii");
     }
 
     public static void duplicateZeros(int[] arr) {
---
+++ Arrays101/Inserting_Item_into_an_Array/MergeSortedArray.java
@@ -0,0 +1,13 @@
+package Inserting_Item_into_an_Array;
+
+public class MergeSortedArray{
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hiii");
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+    }
+}
