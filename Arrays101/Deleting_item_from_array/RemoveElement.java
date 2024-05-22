b4215d80d3a799b48ecd07bc20a1260a4b8246bc
Sahand <sahand.prs@gmail.com>
2024-05-22 12:19:35 +0200

    Done

--- Arrays101/Deleting_item_from_array/RemoveElement.java
+++ Arrays101/Deleting_item_from_array/RemoveElement.java
@@ -8,20 +8,17 @@ public class RemoveElement {
 
     public static int removeElement(int[] nums, int val) {
         int count = 0;
-
+    
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i] != val){
+            if (nums[i] != val) {
                 nums[count++] = nums[i];
             }
         }
-
-        while(count < nums.length){
-            nums[count++] = val;
-        }
-
-
+    
         return count;
     }
+    
+    
 
    
 
