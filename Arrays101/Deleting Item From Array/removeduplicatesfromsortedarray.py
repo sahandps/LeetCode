a33af9f92cc38bf3aace2a6e47fecacb63cfcb9f
Sahand <sahand.prs@gmail.com>
2024-11-20 10:59:14 +0100

    Done

---
+++ Arrays101/Deleting Item From Array/removeduplicatesfromsortedarray.py
@@ -0,0 +1,21 @@
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+
+
+def removeDuplicates(nums):
+    i = 0
+    j = 1
+    
+    while i < j:
+        if nums[i] != nums[j]:
+            i += 1
+            nums[i] = nums[j]
+        j += 1    
+    
+       
+
+        
+
+
+
+removeDuplicates(nums)
