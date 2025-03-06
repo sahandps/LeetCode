59ab5e405237e24bb9246761a5e91411c2070574
Sahand <sahand.prs@gmail.com>
2025-03-06 09:38:11 +0100

    Done with Another one

--- Arrays101/Inserting Item Into An Array/MergeSortedArray.py
+++ Arrays101/Inserting Item Into An Array/MergeSortedArray.py
@@ -4,9 +4,11 @@ def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
     if len(nums2) == 0:
         return nums1
     
-    holder = m - n
+    holder = m + n
     i = 0
-    while holder < len(nums1) - 1:
+    while i < len(nums2) - 1:
         nums2 [holder] = nums1[i]
         holder +=1
-        i += 1 
+        i += 1
+
+    nums1.sort()     
